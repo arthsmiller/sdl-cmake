a273ee1ff39379ac81979093a5e08deeafa1c957
arthsmiller <88456630+arthsmiller@users.noreply.github.com>
2023-11-28 21:51:29 +0100

    + initial commit

---
+++ main.cpp
@@ -0,0 +1,207 @@
+// base stolen from https://github.com/andrew-lim/sdl2-boilerplate
+
+#include <iostream>
+#include <SDL.h>
+#include <boost/random/uniform_int_distribution.hpp>
+#include <boost/random/mersenne_twister.hpp>
+
+#include "main.h"
+
+int main(int argc, char *argv[]) {
+    Game game;
+
+    game.start();
+
+    return 0;
+}
+
+//
+// GAME CLASS DEFINITIONS
+//
+Game::Game()
+    :frameSkip(0), running(0), window(NULL), renderer(NULL) {
+}
+
+Game::~Game() {
+    this->stop();
+}
+
+void Game::start() {
+    int flags = SDL_WINDOW_SHOWN;
+    if (SDL_Init(SDL_INIT_EVERYTHING)) {
+        return;
+    }
+    if (SDL_CreateWindowAndRenderer(DISPLAY_WIDTH, DISPLAY_HEIGHT, flags, &window, &renderer)) {
+        return;
+    }
+    this->running = 1;
+    run();
+}
+
+void Game::draw() {
+    SDL_Rect spriteRect;
+
+    // Clear screen
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
+    // Render hero
+    spriteRect.x = sprite.x;
+    spriteRect.y = sprite.y;
+    spriteRect.w = sprite.width;
+    spriteRect.h = sprite.height;
+    fillRect(&spriteRect, 255, 0, 0);
+
+    SDL_RenderPresent(renderer);
+}
+
+void Game::stop() {
+    if (NULL != renderer) {
+        SDL_DestroyRenderer(renderer);
+        renderer = NULL;
+    }
+    if (NULL != window) {
+        SDL_DestroyWindow(window);
+        window = NULL;
+    }
+    SDL_Quit();
+}
+
+void Game::fillRect(SDL_Rect* rc, int r, int g, int b) {
+    SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+    SDL_RenderFillRect(renderer, rc);
+}
+
+void Game::fpsChanged(int fps) {
+    char szFps[128];
+    sprintf_s(szFps, "%s: %d FPS", "SDL2 Base C++ - Use Arrow Keys to Move", fps);
+    SDL_SetWindowTitle(window, szFps);
+}
+
+void Game::onQuit() {
+    running = 0;
+}
+
+void Game::run() {
+    int past = SDL_GetTicks();
+    int now = past, pastFps = past;
+    int fps = 0, framesSkipped = 0;
+
+    SDL_Event event;
+
+    while (running) {
+        int timeElapsed = 0;
+
+        if (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:    onQuit();            break;
+                case SDL_KEYDOWN: onKeyDown(&event); break;
+                case SDL_KEYUP:   onKeyUp(&event);   break;
+                case SDL_MOUSEBUTTONDOWN: onMouseDown(event.button); break;
+                case SDL_MOUSEBUTTONUP: onMouseUp(); break;
+                case SDL_MOUSEMOTION:
+                    break;
+            }
+        }
+
+        // update/draw
+        timeElapsed = (now = SDL_GetTicks()) - past;
+
+        if (timeElapsed >= UPDATE_INTERVAL) {
+            past = now;
+            update();
+
+            if (framesSkipped++ >= frameSkip) {
+                draw();
+                ++fps;
+                framesSkipped = 0;
+            }
+        }
+
+        // fps
+        if (now - pastFps >= 1000) {
+            pastFps = now;
+            fpsChanged(fps);
+            fps = 0;
+        }
+
+        // sleep?
+        SDL_Delay(1);
+    }
+}
+
+void Game::update() {
+    Helper helper;
+    std::cout << helper.randomInt(1, 10000);
+    returnSpriteToCanvas(sprite);
+
+    if (keys[SDLK_LEFT]) {
+        sprite.x -= HERO_SPEED;
+    }
+    else if (keys[SDLK_RIGHT]) {
+        sprite.x += HERO_SPEED;
+    }
+    else if (keys[SDLK_UP]) {
+        sprite.y -= HERO_SPEED;
+    }
+    else if (keys[SDLK_DOWN]) {
+        sprite.y += HERO_SPEED;
+    }
+    else if (clicks[SDL_BUTTON_LMASK]) {
+        sprite.x -= 10;
+    }
+}
+
+void Game::onKeyDown(SDL_Event* evt) {
+    keys[evt->key.keysym.sym] = 1;
+}
+
+void Game::onKeyUp(SDL_Event* evt) {
+    keys[evt->key.keysym.sym] = 0;
+}
+
+void Game::onMouseDown(SDL_MouseButtonEvent& mouseEvent)
+{
+    if (mouseEvent.button == SDL_BUTTON_LEFT)
+    {
+        clicks[SDL_BUTTON_LMASK] = 1;
+    }
+}
+
+
+void Game::onMouseUp()
+{
+    clicks[SDL_BUTTON_LMASK] = 0;
+    clicks[SDL_BUTTON_MMASK] = 0;
+    clicks[SDL_BUTTON_RMASK] = 0;
+    clicks[SDL_BUTTON_X1MASK] = 0;
+    clicks[SDL_BUTTON_X2MASK] = 0;
+}
+
+void Game::returnSpriteToCanvas(Sprite& sprite)
+{
+    if (sprite.x > DISPLAY_WIDTH){
+        sprite.x = 0;
+    }
+    else if (sprite.x < 0){
+        sprite.x = DISPLAY_WIDTH;
+    }
+    else if (sprite.y > DISPLAY_HEIGHT){
+        sprite.y = 0;
+    }
+    else if (sprite.y < 0){
+        sprite.y = DISPLAY_HEIGHT;
+    }
+}
+
+//
+// HELPER CLASS DEFINITIONS
+//
+int Helper::randomInt(const int min, const int max) {
+    const std::time_t now = std::time(nullptr);
+    boost::random::mt19937 rng(static_cast<unsigned>(now));
+
+    const boost::random::uniform_int_distribution<> generate(min, max);
+
+    return generate(rng);
+}
---
+++ main.h
@@ -0,0 +1,59 @@
+//
+// Created by arthur on 28.11.2023.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <map>
+
+#endif //MAIN_H
+
+enum {
+    DISPLAY_WIDTH = 480,
+    DISPLAY_HEIGHT = 320,
+    UPDATE_INTERVAL = 1000 / 60,
+    HERO_SPEED = 2
+};
+
+class Sprite {
+public:
+    int x, y, width, height;
+    Sprite() :
+        x(0),
+        y(0),
+        width(20),
+        height(20)
+    {}
+};
+
+class Game {
+public:
+    Game();
+    ~Game();
+    void start();
+    void stop();
+    void draw();
+    void fillRect(SDL_Rect* rc, int r, int g, int b);
+    void fpsChanged(int fps);
+    void onQuit();
+    void onKeyDown(SDL_Event* event);
+    void onKeyUp(SDL_Event* event);
+    void onMouseDown(SDL_MouseButtonEvent& mouseEvent);
+    void onMouseUp();
+    void run();
+    void update();
+    void returnSpriteToCanvas(Sprite& sprite);
+private:
+    std::map<int, int> keys; // No SDLK_LAST. SDL2 migration guide suggests std::map
+    std::map<int, int> clicks;
+    int frameSkip;
+    int running;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    Sprite sprite;
+};
+
+class Helper {
+public:
+    int randomInt(int min, int max);
+};
